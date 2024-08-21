5421cf675285c7a8b03bdf17d02ad9c95cd1f848
Rizky Ramadhan <rizkyramadhan@Rizkys-MacBook-Pro.local>
2024-08-21 09:55:42 +0700

    add: axios for get userwa, remove retrieval direct to db

--- messaging.js
+++ messaging.js
@@ -1,8 +1,39 @@
 const http = require('http');
 const https = require('https');
 const axios = require('axios');
+const moment = require('moment')
 
-exports.getAndSend = function (client) {
+function doRequest(options) {
+  return new Promise ((resolve, reject) => {
+    let req = http.request(options);
+
+    req.on('response', res => {
+      res.on('data', d => {
+        process.stdout.write(d);
+        
+        let body = null
+
+        try {
+          body = JSON.parse(d);
+        } catch(e) {
+          console.log("parse error: ", e)
+          reject(e)
+        }
+        
+        console.log(body)
+        resolve(body);
+      })
+    });
+
+    req.on('error', err => {
+      reject(err);
+    });
+
+    req.end();
+  }); 
+}
+
+exports.getAndSend = async function (client) {
   const options = {
     hostname: process.env.API_URL,
     //port: process.env.API_PORT,
@@ -11,39 +42,40 @@ exports.getAndSend = function (client) {
     rejectUnauthorized: false,
   };
 
-  setInterval(function () {
+  setInterval(async function () {
     console.log("get message");
+    let data = null
+    try {
+      data = await doRequest(options)
+    } catch(e) {
+      console.log("error on get message: ", data)
+      return
+    }
     
-    const req = http.request(options, res => {
-      console.log(`statusCode: ${res.statusCode}`);
 
-      res.on('data', d => {
-        process.stdout.write(d);
+    console.log("data:", data)
+    await data.message.forEach(async e => {
+      var number = e.no_hp + "@c.us";
+      var pesan = e.message;
+      // console.log("Send to = " + e.no_hp);
+      // console.log("Message = " + e.message);
 
-        const body = JSON.parse(d);
-        console.log(body);
-        var msg = body.message;
+      console.log(moment().format() + "Send to = " + e.no_hp)
+      console.log(moment().format() + "Message = " + e.message)
 
-        try {
-          for (var i in msg) {
-            var number = msg[i].no_hp + "@c.us";
-            var pesan = msg[i].message;
-            console.log("Send to = " + msg[i].no_hp);
-            console.log("Message = " + msg[i].message);
-
-            // client.sendMessage(number, pesan);
-
-          }
-
-        } catch (err) { console.log(err) }
-
-      });
+      try {
+        await client.sendMessage(number, pesan);
+      } catch (e) {
+        console.log(moment().format() + "error on client send message", e)
+      } 
+      
+      await sleep(3000)
     });
+  }, 1 * 60 * 1000);
+}
 
-    req.on('error', error => {
-      console.error(error);
-    });
-
-    req.end();
-  }, 1 * 30 * 1000);
+function sleep(ms) {
+  return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+  });
 }
